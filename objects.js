fe660d8351622948eb43a3ae66166d4ceefb4cfd
RenataCodes <psych.coaching@gmail.com>
2017-09-19 20:23:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
- return playlist
+  var new = delete playlist.artistName;
+ return new
 }
